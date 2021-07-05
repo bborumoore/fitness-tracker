9818df46c65a5d923de365f44c2aa70dbb254a04
bborumoore <bborumoore@gmail.com>
2021-07-05 08:31:53 -0400

    Fixing deployment issues

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const PORT = 3000;
-
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
